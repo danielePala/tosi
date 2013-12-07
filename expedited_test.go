60e6b0afb2e4c62c49d924757757ecd9a4dd4f4e
Daniele Pala <pala.daniele@gmail.com>
2013-12-07 16:44:04 +0100

    fixed copyright notice for test files

--- expedited_test.go
+++ expedited_test.go
@@ -1,7 +1,7 @@
 /*
  Copyright 2013 Daniele Pala <pala.daniele@gmail.com>
 
- This file is part of
+ This file is part of tosi.
 
  tosi is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
